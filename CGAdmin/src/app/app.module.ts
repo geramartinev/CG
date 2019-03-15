94ef48065bbf294057b7f7db7a9c5512d1c5c124
Gera Mtz <geramartinev@hotmail.com>
2019-03-14 22:42:42 -0600

    Angular material added

--- CGAdmin/src/app/app.module.ts
+++ CGAdmin/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ CGAdmin/src/app/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { 
+    MatButtonModule, 
+    MatSidenavModule, 
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+} from '@angular/material'
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
