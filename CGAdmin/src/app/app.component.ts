b838780c912ddf0545334cfd706684627713f9e8
Gera Mtz <geramartinev@hotmail.com>
2019-03-26 23:07:04 -0600

    appComponent updated to load the shell

--- CGAdmin/src/app/app.component.ts
+++ CGAdmin/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectorRef,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
+
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CGAdmin';
+export class AppComponent implements OnInit, OnDestroy {
+
+  mobileQuery: MediaQueryList;
+  private mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.mobileQueryListener);
+  }
 }
